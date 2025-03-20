1e022c7937a1f299f106e218ad5b4e09ff85f8c8
Eric Rizzi <eric.rizzi@gmail.com>
2025-03-20 12:49:14 -0500

    Update unit tests to previous changes

--- tests/test_problem_01.py
+++ tests/test_problem_01.py
@@ -1,13 +1,13 @@
-from problems.problem_01 import add_two_integers
+from problems.problem_01 import add_length_of_strings
 
 
 def test_add_two_integers_1() -> None:
-    assert add_two_integers(5, 0) == 5
+    assert add_length_of_strings("hi", "there") == 7
 
 
 def test_add_two_integers_2() -> None:
-    assert add_two_integers(5, 4) == 9
+    assert add_length_of_strings("", "") == 0
 
 
 def test_add_two_integers_3() -> None:
-    assert add_two_integers(-1, 8) == 7
+    assert add_length_of_strings("go!", "lab!") == 7
